46548bc57b225a21e9b51d62aeeb6566cbe9e27e
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-02 14:08:11 -0500

    feat: 🎸 add missing icons

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -1,15 +1,45 @@
 import PropTypes from 'prop-types'
 
+import IconArrow from './icons/arrow'
 import IconBadge from './icons/badge'
+import IconBolt from './icons/bolt'
+import IconBriefcase from './icons/briefcase'
 import IconChat from './icons/chat'
+import IconCode from './icons/code'
+import IconFire from './icons/fire'
+import IconGithub from './icons/github'
+import IconGlobe from './icons/globe'
+import IconLinkedIn from './icons/linkedin'
+import IconMailOpen from './icons/mail-open'
 import IconMail from './icons/mail'
+import IconMoon from './icons/moon'
+import IconSpeaker from './icons/speaker'
+import IconStar from './icons/star'
+import IconStorybook from './icons/storybook'
+import IconSun from './icons/sun'
+import IconVolume from './icons/volumne'
 
 import { Component } from './styled'
 
 export const SVG = {
+  arrow: IconArrow,
   badge: IconBadge,
+  bolt: IconBolt,
+  briefcase: IconBriefcase,
   chat: IconChat,
-  mail: IconMail
+  code: IconCode,
+  fire: IconFire,
+  github: IconGithub,
+  globe: IconGlobe,
+  linkedin: IconLinkedIn,
+  mailOpen: IconMailOpen,
+  mail: IconMail,
+  moon: IconMoon,
+  speaker: IconSpeaker,
+  star: IconStar,
+  storybook: IconStorybook,
+  sun: IconSun,
+  volume: IconVolume
 }
 
 const Icon = ({ name, width, fill }) => {
