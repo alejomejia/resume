a342eba8d5d33a106cc35f4e89dd231e15625c04
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-03 10:31:24 -0500

    fix: 🐛 fix bug with api files naming

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -2,47 +2,57 @@ export default function handler(req, res) {
   res.status(200).json([
     {
       id: 1,
-      image: '/experience/freelance.png',
-      title: 'Product Developer / Web Designer',
-      at: 'Freelance',
-      link: '/',
-      startDate: 'January 2013',
-      endDate: 'Today',
-      description:
-        'My work in this nice global company is to build interfaces inside ReactJS. I dont usually work with classes but with hooks. I use libraries like Material UI, Kendo UI and Styled Components. I work with scrum methodology and every day I communicate in English with the team and the client in the United States. I really love how the work in ReactJS can be organized and I always try to make all the components reusable and have all the files and code organized.'
+      title: 'Google Clone',
+      stack: ['NextJS', 'TailwindCSS'],
+      image: '/portfolio/google-clone.png',
+      external: [
+        {
+          id: 1,
+          type: 'repo',
+          link: 'https://github.com/alejomejia/google-clone'
+        },
+        {
+          id: 2,
+          type: 'site',
+          link: 'https://alejomejia-google-clone.vercel.app/'
+        }
+      ]
     },
     {
       id: 2,
-      image: '/experience/making-sense.png',
-      title: 'SSR UX Developer',
-      at: 'MakingSense',
-      link: 'https://makingsense.com/',
-      startDate: 'January 2021',
-      endDate: 'Today',
-      description:
-        'My work in this nice global company is to build interfaces inside ReactJS. I dont usually work with classes but with hooks. I use libraries like Material UI, Kendo UI and Styled Components. I work with scrum methodology and every day I communicate in English with the team and the client in the United States. I really love how the work in ReactJS can be organized and I always try to make all the components reusable and have all the files and code organized.'
+      title: 'Bimbau Design System',
+      stack: ['ReactJS', 'Storybook', 'Styled Components'],
+      image: '/portfolio/bimbau-design-system.png',
+      external: [
+        {
+          id: 1,
+          type: 'site',
+          link: 'https://development.bimbau.co/login'
+        },
+        {
+          id: 2,
+          type: 'storybook',
+          link: 'http://sb.alejandromejia.co/'
+        }
+      ]
     },
     {
       id: 3,
-      image: '/experience/10pearls.png',
-      title: 'UI Developer',
-      at: '10Pearls',
-      link: 'https://10pearls.com/',
-      startDate: 'September 2019',
-      endDate: 'December 2020',
-      description:
-        'My work in this nice global company is to build interfaces inside ReactJS. I dont usually work with classes but with hooks. I use libraries like Material UI, Kendo UI and Styled Components. I work with scrum methodology and every day I communicate in English with the team and the client in the United States. I really love how the work in ReactJS can be organized and I always try to make all the components reusable and have all the files and code organized.'
-    },
-    {
-      id: 4,
-      image: '/experience/integro.png',
-      title: 'UI Developer',
-      at: 'Integ.ro',
-      link: 'https://www.integ.ro/',
-      startDate: 'July 2018',
-      endDate: 'September 2019',
-      description:
-        'My work in this nice global company is to build interfaces inside ReactJS. I dont usually work with classes but with hooks. I use libraries like Material UI, Kendo UI and Styled Components. I work with scrum methodology and every day I communicate in English with the team and the client in the United States. I really love how the work in ReactJS can be organized and I always try to make all the components reusable and have all the files and code organized.'
+      title: 'Social Media Dashboard',
+      stack: ['ReactJS', 'Styled Components', 'React Spring'],
+      image: '/portfolio/social-media-dashboard.png',
+      external: [
+        {
+          id: 1,
+          type: 'repo',
+          link: 'https://github.com/alejomejia/social-media-dashboard'
+        },
+        {
+          id: 2,
+          type: 'site',
+          link: 'https://alejomejia.github.io/social-media-dashboard/'
+        }
+      ]
     }
   ])
 }
