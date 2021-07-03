f862c480ad43f33861e0e78fd8cc73dad89260de
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-03 15:40:13 -0500

    feat: 🎸 add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'styled-components'
+import * as gtag from '../lib/gtag'
 
 import GlobalCSS from '../styles/global'
 import GENERAL from '../styles/themes/general'
 import LIGHT from '../styles/themes/light'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider theme={GENERAL}>
       <ThemeProvider theme={LIGHT}>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
 class MyDocument extends Document {
   render() {
     return (
@@ -12,6 +13,23 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;500;600&display=swap"
             rel="stylesheet"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
           />
         </Head>
         <body>
