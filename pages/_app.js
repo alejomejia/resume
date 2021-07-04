68b89c51bf8c29e29dfa85dd99fbb17b688aa109
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-03 20:51:00 -0500

    feat: 🎸 add Dark theme support

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@ import { ThemeProvider } from 'styled-components'
 import GlobalCSS from '../styles/global'
 import GENERAL from '../styles/themes/general'
 import LIGHT from '../styles/themes/light'
+import DARK from '../styles/themes/dark'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={GENERAL}>
-      <ThemeProvider theme={LIGHT}>
+      <ThemeProvider theme={DARK}>
         <GlobalCSS />
         <Component {...pageProps} />
       </ThemeProvider>
---
+++ styles/themes/dark.js
@@ -0,0 +1,18 @@
+const dark = {
+  colors: {
+    surface: 'hsl(222, 20%, 17%)',
+    bg: 'hsl(221, 22%, 14%)',
+    bgAccent: 'hsl(222, 20%, 13%)',
+    tag: 'hsl(216, 19%, 22%)',
+    cta: 'hsl(195, 100%, 50%)',
+    text100: 'hsl(211, 8%, 50%)',
+    text200: 'hsl(210, 14%, 66%)',
+    text300: 'hsl(204, 24%, 96%)'
+  },
+  shadows: {
+    card: '0 1px 3px hsla(0, 0%, 0%, 0.25)',
+    fixed: '0 2px 12px hsla(0, 0%, 0%, 0.25)'
+  }
+}
+
+export default dark
