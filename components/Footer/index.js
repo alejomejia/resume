0a7d9bc8f27c05eb71ae83122d2ae8d5b9c1c88a
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-03 20:33:49 -0500

    style: 💄 add Footer extra styles

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,13 +7,15 @@ const Footer = () => {
         <span className="footer__text">
           Not convinced yet?{' '}
           <a className="footer__link" href="tel:+573192160752">
-            Let's talk
+            Let&apos;s talk
           </a>{' '}
           ⚡
         </span>
       </div>
       <div className="footer__bottom">
-        <span>Made with NextJS and Styled Components 💙</span>
+        <span>
+          Designed in Figma, built with NextJS and Styled Components 💙
+        </span>
       </div>
     </Component>
   )
