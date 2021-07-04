7a6c414e652a389034d6620cea21e7d5e52670a3
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-04 10:46:47 -0500

    feat: 🎸 add microinteraction to volume toggler

--- components/Icon/icons/volume.js
+++ components/Icon/icons/volume.js
@@ -1,11 +1,37 @@
+import { useContext } from 'react'
+import { store } from 'context/store'
+import { useSpring, animated } from 'react-spring'
+
 const IconVolume = () => {
+  const { isSoundEnable } = useContext(store)
+
+  const inside = useSpring({
+    opacity: isSoundEnable ? 1 : 0,
+    delay: isSoundEnable ? 0 : 150
+  })
+  const outside = useSpring({
+    opacity: isSoundEnable ? 1 : 0,
+    delay: isSoundEnable ? 150 : 0
+  })
+
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path
-        fillRule="evenodd"
-        d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
-        clipRule="evenodd"
+        id="horn"
+        d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217z"
       />
+      <animated.g style={outside}>
+        <path
+          id="outside"
+          d="M15.364 2.636a1 1 0 00-.707 1.707A7.97 7.97 0 0117 10a7.972 7.972 0 01-2.343 5.657 1 1 0 001.414 1.414A9.972 9.972 0 0019 10a9.973 9.973 0 00-2.929-7.071 1 1 0 00-.707-.293z"
+        />
+      </animated.g>
+      <animated.g style={inside}>
+        <path
+          id="inside"
+          d="M12.153 5.54a1 1 0 00-.325 1.632A3.982 3.982 0 0113 10a3.984 3.984 0 01-1.172 2.828 1 1 0 001.415 1.415A5.984 5.984 0 0015 10a5.984 5.984 0 00-1.757-4.243 1 1 0 00-1.09-.217z"
+        />
+      </animated.g>
     </svg>
   )
 }
