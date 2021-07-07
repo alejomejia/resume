e7a3108a06fc11c41cb5cf1da7b4231753d27c69
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-07-07 17:54:25 -0500

    feat: 🎸 add social media meta tags

--- components/Head/index.jsx
+++ components/Head/index.jsx
@@ -11,6 +11,17 @@ const Head = ({ title }) => {
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, 
      user-scalable=0"
       />
+      {/* Social Tags */}
+      <meta property="og:title" content="Alejandro Mejia Resume" />
+      <meta
+        property="og:image"
+        content="https://resume.alejandromejia.co/social-media-cover.png"
+      />
+      <meta
+        property="og:description"
+        content="Hey 👋🏻! I'm Product Developer with 8 years of coding experience on the UI side"
+      />
+      <meta property="og:url" content="https://resume.alejandromejia.co" />
     </HTMLHead>
   )
 }
