5126f6ad35af2023a12c28333dc56de17fb2033f
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-10-01 14:10:42 -0500

    refactor: 💡 refactor Button with better styled-comp practices

--- components/Button/styled.js
+++ components/Button/styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-export const Component = styled.button`
+export const Wrapper = styled.button`
   cursor: pointer;
   display: flex;
   align-items: center;
-  padding: ${({ theme }) => theme.spacing(1)} ${({ theme }) => theme.spacing(2)};
+  padding: ${({ theme }) => `${theme.spacing(1)} ${theme.spacing(2)}`};
   color: var(--color-white);
   font-size: ${({ theme }) => theme.fonts.size.m};
   font-family: ${({ theme }) => theme.fonts.family.primary};
@@ -14,19 +14,17 @@ export const Component = styled.button`
   letter-spacing: 1px;
   border: 0;
   border-radius: 8px;
+`
 
-  .button {
-    &__text {
-      display: inline-block;
-      margin-right: ${({ theme }) => theme.spacing(1)};
-    }
+export const Text = styled.span`
+  display: inline-block;
+  margin-right: ${({ theme }) => theme.spacing(1)};
+`
 
-    &__icon {
-      display: inline-flex;
+export const IconWrapper = styled.span`
+  display: inline-flex;
 
-      svg {
-        fill: var(--color-white);
-      }
-    }
+  svg {
+    fill: var(--color-white);
   }
 `
