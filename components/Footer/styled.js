3f3dbb8bf09e48a2f17ad1d88dfa03ed7c0a57b5
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-10-02 08:45:43 -0500

    refactor: 💡 refactor Footer with better styled-comp practices

--- components/Footer/styled.js
+++ components/Footer/styled.js
@@ -1,75 +1,73 @@
 import styled from 'styled-components'
 
-export const Component = styled.footer`
-  .footer {
-    &__top {
-      display: flex;
-      padding-left: ${({ theme }) => theme.spacing(3)};
-      padding-right: ${({ theme }) => theme.spacing(3)};
-      margin-top: ${({ theme }) => theme.spacing(12.5)};
-      margin-bottom: ${({ theme }) => theme.spacing(12.5)};
-      justify-content: center;
-      font-family: ${({ theme }) => theme.fonts.family.primary};
+export const Wrapper = styled.footer``
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
-        margin-top: ${({ theme }) => theme.spacing(8)};
-        margin-bottom: ${({ theme }) => theme.spacing(8)};
-        padding-left: ${({ theme }) => theme.spacing(4)};
-        padding-right: ${({ theme }) => theme.spacing(4)};
-      }
+export const FooterTop = styled.div`
+  display: flex;
+  padding-left: ${({ theme }) => theme.spacing(3)};
+  padding-right: ${({ theme }) => theme.spacing(3)};
+  margin-top: ${({ theme }) => theme.spacing(12.5)};
+  margin-bottom: ${({ theme }) => theme.spacing(12.5)};
+  justify-content: center;
+  font-family: ${({ theme }) => theme.fonts.family.primary};
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.xxs}) {
-        margin-top: ${({ theme }) => theme.spacing(6)};
-        margin-bottom: ${({ theme }) => theme.spacing(6)};
-        padding-left: ${({ theme }) => theme.spacing(2)};
-        padding-right: ${({ theme }) => theme.spacing(2)};
-      }
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+    margin-top: ${({ theme }) => theme.spacing(8)};
+    margin-bottom: ${({ theme }) => theme.spacing(8)};
+    padding-left: ${({ theme }) => theme.spacing(4)};
+    padding-right: ${({ theme }) => theme.spacing(4)};
+  }
 
-    &__text {
-      display: flex;
-      align-items: center;
-      gap: 8px;
-      color: var(--color-text-200);
-      font-size: ${({ theme }) => theme.fonts.size.xl};
+  @media (max-width: ${({ theme }) => theme.breakpoints.xxs}) {
+    margin-top: ${({ theme }) => theme.spacing(6)};
+    margin-bottom: ${({ theme }) => theme.spacing(6)};
+    padding-left: ${({ theme }) => theme.spacing(2)};
+    padding-right: ${({ theme }) => theme.spacing(2)};
+  }
+`
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
-        font-size: ${({ theme }) => theme.fonts.size.l};
-      }
+export const Text = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: var(--color-text-200);
+  font-size: ${({ theme }) => theme.fonts.size.xl};
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
-        font-size: ${({ theme }) => theme.fonts.size.m};
-      }
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    font-size: ${({ theme }) => theme.fonts.size.l};
+  }
 
-    &__link {
-      color: var(--color-primary);
-      font-weight: ${({ theme }) => theme.fonts.weight.bold};
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    font-size: ${({ theme }) => theme.fonts.size.m};
+  }
+`
 
-    &__bottom {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      padding-left: ${({ theme }) => theme.spacing(3)};
-      padding-right: ${({ theme }) => theme.spacing(3)};
-      min-height: 60px;
-      color: var(--color-text-100);
-      font-family: ${({ theme }) => theme.fonts.family.primary};
-      background-color: var(--color-bg-accent);
+export const Link = styled.a`
+  color: var(--color-primary);
+  font-weight: ${({ theme }) => theme.fonts.weight.bold};
+`
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
-        font-size: ${({ theme }) => theme.fonts.size.sm};
-      }
+export const FooterBottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: ${({ theme }) => theme.spacing(3)};
+  padding-right: ${({ theme }) => theme.spacing(3)};
+  min-height: 60px;
+  color: var(--color-text-100);
+  font-family: ${({ theme }) => theme.fonts.family.primary};
+  background-color: var(--color-bg-accent);
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
-        padding-top: ${({ theme }) => theme.spacing(3)};
-        padding-bottom: ${({ theme }) => theme.spacing(12)};
-      }
+  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+    font-size: ${({ theme }) => theme.fonts.size.sm};
+  }
 
-      @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
-        padding-bottom: ${({ theme }) => theme.spacing(8)};
-      }
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+    padding-top: ${({ theme }) => theme.spacing(3)};
+    padding-bottom: ${({ theme }) => theme.spacing(12)};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    padding-bottom: ${({ theme }) => theme.spacing(8)};
   }
 `
