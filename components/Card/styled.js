8989b078e024234e56f4ee5fd2d7b4deb32b4f59
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-10-03 08:51:04 -0500

    refactor: 💡 improve ternary overall styled ternary conditions

--- components/Card/styled.js
+++ components/Card/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.article`
-  margin-bottom: ${(p) => (p.$mb ? p.$mb : p.theme.spacing(2.5))};
+  margin-bottom: ${(p) => p.$mb || p.theme.spacing(2.5)};
   padding: ${({ theme }) => theme.spacing(3)};
   background-color: var(--color-surface);
   border-radius: 8px;
--- components/Heading/styled.js
+++ components/Heading/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.h1`
   display: flex;
@@ -12,11 +12,16 @@ export const IconWrapper = styled.span`
 `
 
 export const Title = styled.span`
-  color: ${(p) => (p.$color ? p.$color : 'var(--color-text-300)')};
-  font-size: ${(p) => (p.$size ? p.$size : p.theme.fonts.size.xl)};
+  color: ${(p) => p.$color || 'var(--color-text-300)'};
+  font-size: ${(p) => p.$size || p.theme.fonts.size.xl};
   font-family: ${({ theme, family }) => theme.fonts.family[family]};
-  font-weight: ${(p) => (p.$weight ? p.$weight : p.theme.fonts.weight.bold)};
-  font-style: ${(p) => (p.$isItalic ? 'italic' : '')};
+  font-weight: ${(p) => p.$weight || p.theme.fonts.weight.bold};
+
+  ${(p) =>
+    p.$isItalic &&
+    css`
+      font-style: italic;
+    `}
 
   h3${Wrapper} & {
     @media (max-width: ${({ theme }) => theme.breakpoints.xxs}) {
